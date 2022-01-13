72d30e5e9a5b0fdd7bea426f059f00e3575301fe
aeskafi <arham.eskafi@gmail.com>
2022-01-13 19:06:46 +0300

    Update: favIcon and mainBoard.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainBoard from './mainBoard';
+import './styles.css';
 
-ReactDOM.render(<div />, document.getElementById('mainBoard'));
+ReactDOM.render(<MainBoard />, document.getElementById('mainBoard'));
