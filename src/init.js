7aa8ed6f23898d268b333cf26f87451b4c4b85b2
aeskafi <arham.eskafi@gmail.com>
2022-01-14 12:12:41 +0300

    Update: import p5 library for drawing in react with "react-p5"

---
+++ src/init.js
@@ -0,0 +1,30 @@
+export const CANVAS_SIZE = [600, 540]
+export const SCALE = 20
+export const SPEED = 130
+export const COLORS = ['#ecb5ff',
+    '#ffa0ab',
+    '#8cffb4',
+    '#ff8666',
+    '#80c3f5',
+    '#c2e77d',
+    '#fdf9a1',]
+export const DARK_COLOR = "#071820"
+export const LIGHT_COLOR = "#344c57"
+export const BG_COLOR = "#ecf4cb"
+export const GRID_SPACE = 30
+export const GAME_EDGE_LEFT = 150
+export const GAME_EDGE_RIGHT = 450
+// var gridPieces = [];
+// var lineFades = [];
+// var gridWorkers = [];
+
+// var currentScore = 0;
+// var currentLevel = 1;
+// var linesCleared = 0;
+
+// var ticks = 0;
+// var updateEvery = 15;
+// var updateEveryCurrent = 15;
+// var fallSpeed = gridSpace * 0.5;
+// var pauseGame = false;
+// var gameOver = false;
--- src/mainBoard.js
+++ src/mainBoard.js
@@ -1,11 +1,58 @@
-import React, {Component} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import {
+    CANVAS_SIZE,
+    SCALE,
+    COLORS,
+    DARK_COLOR,
+    LIGHT_COLOR,
+    BG_COLOR,
+    GAME_EDGE_LEFT,
+    GAME_EDGE_RIGHT,
+} from './init';
+import Sketch from "react-p5";
 
-export default class mainBoard extends Component {
-    render() {
-        return (
-            <div>
+let x = 50;
+let y = 50;
 
-            </div>
-        )
+const MainBoard = () => {
+
+    const drawRight = (p5) => {
+        // Right side information box drawing
+        p5.background(BG_COLOR);
+        p5.fill(25);
+        p5.noStroke();
+        p5.rect(GAME_EDGE_RIGHT, 0, 150, CANVAS_SIZE[0])
+        p5.fill(BG_COLOR)
+        //Score rectangle
+        p5.rect(450, 50, 150, 70);
+        //Next piece rectangle
+        p5.rect(460, 300, 130, 130, 5, 5);
+        //Level rectangle
+        p5.rect(460, 130, 130, 60, 5, 5);
+        //Lines rectangle
+        p5.rect(460, 200, 130, 60, 5, 5);
     }
+
+    const drawLeft = (p5) => {
+        // Left side information box drawing
+        p5.fill(25);
+        p5.noStroke();
+        p5.rect(0, 0, GAME_EDGE_LEFT, CANVAS_SIZE[0])
+    }
+
+    const setup = (p5, canvasParentRef) => {
+        p5.createCanvas(CANVAS_SIZE[0], CANVAS_SIZE[1]).parent(canvasParentRef)
+    };
+
+    const draw = (p5) => {
+        drawRight(p5)
+        drawLeft(p5)
+    };
+
+
+    return (
+        <Sketch setup={setup} draw={draw} />
+    )
 }
+
+export default MainBoard;
